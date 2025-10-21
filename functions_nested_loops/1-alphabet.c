be901c73d3c8958e4b0bd3c8e581840d461b8f2a
Aythann <aythancristovao@gmail.com>
2025-10-21 15:10:16 +0200

    Ajout du fichier 1-alphabet.c : définit la fonction print_alphabet qui affiche l’alphabet en minuscules avec _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char n = 'a';
+
+	for (; n <= 'z'; n++)
+	_putchar(n);
+
+	_putchar('\n');
+}
