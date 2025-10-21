b6352e9ff1d3a79bbdd81e45547a9714425f002e
Aythann <aythancristovao@gmail.com>
2025-10-21 10:29:24 +0200

    Ajout du fichier 8-print_base16.c : affiche les chiffres hexadécimaux de 0 à f avec putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char n;
+
+	n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'a';
+	while (n <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
