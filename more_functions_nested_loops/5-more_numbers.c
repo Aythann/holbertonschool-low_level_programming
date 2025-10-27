d40e165ede890520ece5b41aa00440ab6118d4c4
Aythann <aythancristovao@gmail.com>
2025-10-27 11:34:58 +0100

    Ajout de la fonction more_numbers pour afficher 10 fois les nombres de 0 à 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,20 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  *
- *
  * Return: void
  */
 void more_numbers(void)
 {
-	int l;
-	int c;
+	int l, c;
 
-	for (l = 0; l <= 9; l++)
+	for (l = 0; l < 10; l++)
 	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar('1');
+			_putchar('0' + (c % 10));
+		}
 		_putchar('\n');
-		for (c = 0 ; c <= 14; c++)
-		_putchar('0' + c);
 	}
-	_putchar('\n');
 }
