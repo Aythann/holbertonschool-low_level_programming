4a97b4cf48b944a4274ced71c4fa80c24f7887da
Aythann <aythancristovao@gmail.com>
2025-10-27 15:04:22 +0100

    Ajout de la fonction print_line pour tracer une ligne horizontale avec _

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,13 @@
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
-		for (n = 0; n < n; n++)
+		for (i = 0; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
