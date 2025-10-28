f1f46e639edbf06e3c4a47129ab6646807abfad0
Aythann <aythancristovao@gmail.com>
2025-10-28 11:37:02 +0100

    Ajout de la fonction _strlen pour retourner la longueur d'une chaîne de caractères

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -8,13 +8,13 @@
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*s != '\0')
-    {
+	while (*s != '\0')
+	{
 		len++;
-        s++;
-    }
+		s++;
+	}
 
-    return (len);
+	return (len);
 }
