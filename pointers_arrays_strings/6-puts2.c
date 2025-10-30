4fd866fca32c1995f2cea58901a4ed2c660568ae
Aythann <aythancristovao@gmail.com>
2025-10-30 17:24:27 +0100

    feat: implement puts2 to print every other character starting at index 0

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first one
+ * @str: Pointer to the string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	if (!str)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+			break;
+		i += 2;
+	}
+	_putchar('\n');
+}
+
+
