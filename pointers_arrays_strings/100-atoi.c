d9f1e4439f260f031beb37f0a7e9ba8575b32e6c
Aythann <aythancristovao@gmail.com>
2025-10-31 15:26:44 +0100

    Ajout de la fonction _atoi pour convertir une chaîne en entier avec gestion du signe

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,32 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: Pointer to the string to be converted
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
  *
- * Description: Takes into account all '+' and '-' signs before the number.
- * If there are no numbers in the string, returns 0.
- *
- * Return: The converted integer value
+ * Return: integer value of the string, or 0 if no numbers
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
+	unsigned int result = 0;
 	int started = 0;
-	int result = 0;
 
-	if (!s)
-		return (0);
-
-	while (s[i] != '\0' && !started)
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign = -sign;
-		else if (s[i] == '+')
-		else if (s[i] >= '0' && s[i] <= '9')
-			started = 1;
-		else
-		if (!started)
-			i++;
-	}
+			sign *= -1;
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		int digit = s[i] - '0';
-		result = result * 10 + digit;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+
+		else if (started)
+			break;
+
 		i++;
 	}
 
