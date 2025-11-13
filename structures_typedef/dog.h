b9da5221e3841ae162dd16a1de5c344d745e9a2c
Aythann <aythancristovao@gmail.com>
2025-11-13 18:54:21 +0100

    Add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure describing a dog
+ * @name: Pointer to the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
