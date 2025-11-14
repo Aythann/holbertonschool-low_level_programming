fd0683d95ded651e58c2dff3f5104e955fb669b7
Aythann <aythancristovao@gmail.com>
2025-11-14 15:59:58 +0100

    Exercice 4 : ajout de la fonction new_dog (4-new_dog.c)

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-        i++;
+	while (s[i])
+		i++;
 
-    return (i);
+	return (i);
 }
 
 /**
@@ -24,16 +24,16 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
 
 /**
@@ -45,37 +45,37 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    int len1, len2;
+	dog_t *d;
+	int len1, len2;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-    len1 = _strlen(name);
-    len2 = _strlen(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
-    d->name = malloc(sizeof(char) * (len1 + 1));
-    if (d->name == NULL)
-    {
-        free(d);
-        return (NULL);
-    }
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 
-    d->owner = malloc(sizeof(char) * (len2 + 1));
-    if (d->owner == NULL)
-    {
-        free(d->name);
-        free(d);
-        return (NULL);
-    }
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 
-    _strcpy(d->name, name);
-    _strcpy(d->owner, owner);
-    d->age = age;
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+	d->age = age;
 
-    return (d);
+	return (d);
 }
