3bfa5e817552ed161e457fe035a823ad9fdf5b7b
Aythann <aythancristovao@gmail.com>
2025-11-20 10:22:04 +0100

    Ajoute l’implémentation correcte de print_name avec vérification des paramètres

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name using a callback function
@@ -10,8 +9,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
-
     f(name);
 }
