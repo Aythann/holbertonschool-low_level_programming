4e10d7ffd36798d7bb28b6ea51097ab17f5a4460
Aythann <aythancristovao@gmail.com>
2025-11-20 11:12:22 +0100

    Ajoute get_op_func : sélectionne l'opération appropriée via la structure op_t

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * get_op_func - selects the correct operation
  * @s: operator passed as argument
  *
- * Return: pointer to function that matches operator, or NULL
+ * Return: pointer to correct function, or NULL
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -21,9 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s && s[1] == '\0')
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }
